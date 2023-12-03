5879181cf2463f5a3ab4e3013c4d3ac69c99dba0
Nagib <131501717+NicolasNagib653@users.noreply.github.com>
2023-12-02 21:49:43 -0300

    comecei um novo projeto

---
+++ desafios/d001/js/calculadora.js
@@ -0,0 +1,99 @@
+
+var numeros = document.getElementById('numeros')
+
+function add1(){
+    if(numeros.value == 0){
+        numeros.innerHTML =  1
+    }
+    else{
+    numeros.innerHTML = numeros.value + 1
+    }
+}
+function add2(){
+    
+    if(numeros.value == 0){
+        numeros.innerHTML =  2
+    }
+    else{
+    numeros.innerHTML = numeros.value + 2
+    }
+}
+function add3(){
+   
+    if(numeros.value == 0){
+        numeros.innerHTML =  3
+    }
+    else{
+    numeros.innerHTML = numeros.value + 3
+    }
+}
+function add4(){
+    if(numeros.value == 0){
+        numeros.innerHTML =  4
+    }
+    else{
+    numeros.innerHTML = numeros.value + 4
+    }
+}
+function add5(){
+    
+    if(numeros.value == 0){
+        numeros.innerHTML =  5
+    }
+    else{
+    numeros.innerHTML = numeros.value + 5
+    }
+}
+function add6(){
+   
+    if(numeros.value == 0){
+        numeros.innerHTML =  6
+    }
+    else{
+    numeros.innerHTML = numeros.value + 6
+    }
+}
+function add7(){
+    if(numeros.value == 0){
+        numeros.innerHTML = 7
+    }
+    else{
+    numeros.innerHTML = numeros.value + 7
+    }
+}
+function add8(){
+    
+    if(numeros.value == 0){
+        numeros.innerHTML = 8
+    }
+    else{
+    numeros.innerHTML = numeros.value + 8
+    }
+}
+function add9(){
+   
+    if(numeros.value == 0){
+        numeros.innerHTML =  9
+    }
+    else{
+    numeros.innerHTML = numeros.value + 9
+    }
+}
+function add9(){
+    if(numeros.value == 0){
+        numeros.innerHTML =  9
+    }
+    else{
+    numeros.innerHTML = numeros.value + 9
+    }
+}
+function add0(){
+    
+    if(numeros.value == 0){
+        numeros.innerHTML =  0
+    }
+    else{
+    numeros.innerHTML = numeros.value + 0
+    }
+}
+
