0f3854f38777414075eca62d97e767569602ee42
Nagib <131501717+NicolasNagib653@users.noreply.github.com>
2023-12-04 21:13:08 -0300

    declarei mais algumas funções na calculadora

--- desafios/d001/js/calculadora.js
+++ desafios/d001/js/calculadora.js
@@ -1,6 +1,7 @@
 
-var numeros = document.getElementById('numeros')
-
+var numeros = document.getElementById("numeros")
+var soma1 = 0
+var n1, sub1, divi1,porcen1,multi1   = 0
 function add1(){
     if(numeros.value == 0){
         numeros.innerHTML =  1
@@ -97,3 +98,88 @@ function add0(){
     }
 }
 
+function limpar(){
+    numeros.innerHTML = 0
+}
+
+function soma(){
+    n1 = numeros.value 
+    numeros.value += "+"
+    soma1 = 1
+}
+
+function sub(){
+    n1 = numeros.value
+    numeros.value += "-"
+    sub1 = 2
+}
+
+function multi(){
+    n1 = numeros.value
+    numeros.value += "X"
+    multi1 = 3
+}
+
+function divi(){
+    n1 = numeros.value
+    numeros.value += "÷"
+    divi1 = 4
+}
+
+function porcen(){
+    n1 = numeros.value
+    numeros.value += "%"
+    porcen1 = 5
+}
+
+function parentE(){
+    if(numeros.value == 0){
+        numeros.innerHTML =  "("
+    }
+    else{
+        numeros.value += "("
+    }
+    resp = 6
+}
+function parentD(){
+    if(numeros.value == 0){
+        numeros.innerHTML =  ")"
+    }
+    else{
+        numeros.value += ")"
+    }
+}
+
+function igual(){
+    if(soma1 == 1){
+        let str = String(numeros.value)
+        let n2 = str.split("+")
+        numeros.innerHTML = Number(n1) + Number(n2[1])
+    }else
+    if(sub1 == 2){
+        let str = String(numeros.value)
+        let n2 = str.split("-")
+        numeros.innerHTML = Number(n1) - Number(n2[1])
+    }else
+    if(multi1 == 3){
+        let str = String(numeros.value)
+        let n2  = str.split("X")
+        numeros.innerHTML = Number(n1) * Number(n2[1])
+    }
+    if(divi1 == 4){
+        let str = String(numeros.value)
+        let n2  = str.split("÷")
+        numeros.innerHTML = Number(n1) / Number(n2[1])
+    }
+    if(porcen1 == 5){
+        let str = String(numeros.value)
+        let n2  = str.split("%")
+        console.log(n2)
+        if(n2[1] == ""){
+            numeros.innerHTML = parseFloat(n1) / 100
+        } else{
+        numeros.innerHTML = parseFloat(n1) * parseFloat(n2[1]) / 100
+        }
+    }
+
+}
