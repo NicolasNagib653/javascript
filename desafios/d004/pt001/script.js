cee75d8909adb2363d0c979c744c52ab88392445
Nagib <131501717+NicolasNagib653@users.noreply.github.com>
2024-05-24 20:46:08 -0300

    finalizei o exercicio de contador de passos

--- desafios/d004/pt001/script.js
+++ desafios/d004/pt001/script.js
@@ -1,15 +1,31 @@
-function contar(){
-    var inicio = document.getElementById('inicio')
-    var fim    = document.getElementById('fim')
-    var passo  = document.getElementById('passo')
-    var res    = document.getElementById('res')
-    var c = inicio.value
+function contar() {
+  var inicio = document.getElementById('inicio')
+  var fim = document.getElementById('fim')
+  var passo = document.getElementById('passo')
+  var res = document.getElementById('res')
+  var c = inicio.value
 
-  let i = Number(inicio.value)
-  let f = Number(fim.value)
-  let p = Number(passo.value)
+  
 
-  for(let c = i; i <= f; i = i + p){
-    res.innerHTML += `${i}...`
+  let ini = Number(inicio.value)
+  let fin = Number(fim.value)
+  let pas = Number(passo.value)
+
+  if(pas == 0){
+    pas = 1
+  }
+
+  if (inicio.value == "" || fim.value == ""){
+    res.innerHTML = "Não é possivel realizar a contagem de passos, pois um ou mais campos está vazio"
+  } else{
+    if (ini < fin) {
+      for (let c = ini; ini <= fin; ini = ini + pas) {
+        res.innerHTML += `${ini}... `
+      }
+    } else if (ini > fin) {
+      for (let c = ini; ini >= fin; ini = ini - pas) {
+        res.innerHTML += `${ini}... `
+      }
+    }
   }
 }
